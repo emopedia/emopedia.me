83bb23e14e9fee6e052b618a4e3b5915148b6a17
emopedia <76584247+emopedia@users.noreply.github.com>
2021-06-25 19:32:32 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,118 @@
-document.addEventListener('DOMContentLoaded', () => {
-	if (location.pathname == '/') {
-		// Typer
-		var words = ['too many projects', 'minecraft plugins', 'web development', 'game development', 'frontend development'];
-		var currentPart = 0;
-		var partIndex = 0;
-		var typer;
-		var work = document.querySelector('#work');
-		var cursor = document.querySelector('#cursor');
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " ";
+    var clock = document.getElementById("clock");
+    if (!clock) return;
+    clock.innerText = time;
+    clock.textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
 
-		function type() {
-			cursor.style.display = 'inline-block';
-			var text = words[currentPart].substring(0, partIndex + 1);
-			work.innerHTML = text;
-			partIndex++;
+showTime();
 
-			if (text === words[currentPart]) {
-				clearInterval(typer);
-				setTimeout(function () {
-					cursor.style.display = 'none';
-					typer = setInterval(deleteText, 50);
-				}, 3000);
-			}
-		}
+function footer() {
+    var year = new Date().getFullYear();
+    var footer = document.getElementById("footer");
+    footer.innerHTML = `&copy <a href="" class="footer text">Obey</a> ${year}`
+}
 
-		function deleteText() {
-			var text = words[currentPart].substring(0, partIndex - 1);
-			work.innerHTML = text;
-			partIndex--;
+function changeTheme() {
+    var wrapper = document.getElementById("wrapper");
+    var themechanger = document.getElementById("themechanger");
+    var content = document.getElementById("content");
+    if (wrapper.classList.contains("purple")) {
+        console.log('Theme: Purple');
+        setCookie("theme", "gray", 7);
+        content.classList.add("fade");
+        setTimeout(() => {
+            wrapper.classList.remove("purple");
+            wrapper.classList.add("gray");
+            themechanger.innerHTML = "<i class='fas fa-sun fa-flip-horizontal'></i>"
+            if (!wrapper.classList.contains("gray")) return;
+            content.classList.remove("fade");
+        }, 1000);
+    } else {
+        console.log('Theme: Gray');
+        setCookie("theme", "purple", 7);
+        content.classList.add("fade");
+        setTimeout(() => {
+            wrapper.classList.remove("gray");
+            wrapper.classList.add("purple");
+            themechanger.innerHTML = "<i class='fas fa-moon fa-flip-horizontal'></i>"
+            if (!wrapper.classList.contains("purple")) return;
+            content.classList.remove("fade");
+        }, 1000);
+    };
+}
 
-			if (text === '') {
-				clearInterval(typer);
+function checktheme() {
+    var wrapper = document.getElementById("wrapper");
+    var themechanger = document.getElementById("themechanger");
+    var content = document.getElementById("content");
+    var theme = getCookie("theme");
+    if (!theme) setCookie("theme", "purple", 1);
+    console.log(theme);
+    if (theme == "gray") {
+        console.log('Theme: Gray')
+        wrapper.classList.add("gray");
+        themechanger.innerHTML = "<i class='fas fa-sun fa-flip-horizontal'></i>";
+    } else {
+        console.log('Theme: Purple')
+        wrapper.classList.add("purple");
+        themechanger.innerHTML = "<i class='fas fa-moon fa-flip-horizontal'></i>";
+    }
+}
 
-				if (currentPart == words.length - 1) currentPart = 0;
-				else currentPart++;
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-				partIndex = 0;
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
 
-				setTimeout(function () {
-					typer = setInterval(type, 100);
-				}, 200);
-			}
-		}
+function setAge() {
+    birthDate = new Date("06 January 2003");
+    otherDate = new Date();
+    element = document.getElementById("dynamicAge");
 
-		typer = setInterval(type, 100);
-	}
-});
+    if (!element) return;
+
+    var years = (otherDate.getFullYear() - birthDate.getFullYear());
+
+    if (otherDate.getMonth() < birthDate.getMonth() || 
+        otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+        years--;
+    }
+
+    element.innerHTML = years;
+}
